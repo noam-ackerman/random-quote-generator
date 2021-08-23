a758f9e983912b1343a59e4f54a206ac597da722
noam-ackerman <noamac1812@gmail.com>
2021-08-23 22:44:58 +0200

    adding elements, style and random quote pick and display function

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+//selectors
+
+let btn = document.querySelector(".btn");
+let quote = document.querySelector(".quote");
+let source = document.querySelector(".name");
+let resultContainer = document.querySelector(".result");
+
+//eventListeners
+
+btn.addEventListener("click", getQuotes);
+
 function displayQuotes(response) {
-  console.log(response.data);
+  let quotesArr = response.data;
+  let randomQuote = quotesArr[Math.floor(Math.random() * quotesArr.length)];
+  quote.innerHTML = `"${randomQuote.quote}"`;
+  source.innerHTML = randomQuote.name;
+  resultContainer.style.background = "#ff35c5";
 }
 
 function getQuotes() {
   let apiUrl = "https://my-qoutes-api.herokuapp.com/qoutes";
   axios.get(apiUrl).then(displayQuotes);
 }
-
-getQuotes();
